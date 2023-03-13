30c3655228b0f8cde77d6a98f2ff8e5c691e0ff7
loic rouzaud <loic.rouzaud@epitech.eu>
2023-03-13 17:17:27 +0100

    [ADD] - test of recorder

---
+++ audioPoc/PortAudio/Recorder.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sounddevice as sd
+import numpy as np
+
+# Load the raw audio file
+raw_audio_file = 'recorded.raw'
+with open(raw_audio_file, 'rb') as f:
+    raw_audio = np.frombuffer(f.read(), dtype=np.float32)
+
+# Play the audio back
+sd.play(raw_audio, samplerate=44100)
+sd.wait()
